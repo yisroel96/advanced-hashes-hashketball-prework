b5af0aa983c85b543e9e6236128a14c35f33933b
witty-collection-3981 <yisroel96@gmail.com>
2018-11-28 20:16:53 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -118,7 +118,60 @@ def game_hash
   }
 end
 
+def shoe_size(name)
+  player = find_the_player(name)
+  player.fetch(:shoe)
+end
+
 def num_points_scored(name)
   player = find_the_player(name)
   player.fetch(:points)
+end
+
+def team_colors(team_name)
+  team = find_the_team(team_name)
+  team.fetch(:colors)
+end
+
+def teams
+  game_hash.values
+end
+
+def find_the_team(team_name)
+  teams.find {|team| team.fetch(:team_name) == team_name}
+end
+
+def big_shoe_rebounds
+  player = player_biggest_shoe_size
+  player.fetch(:rebounds)
+end
+
+def player_biggest_shoe_size
+  players.sort_by {|player| player.fetch(:shoe) }.last
+end
+
+def players
+  home_players = game_hash.fetch(:home).fetch(:players)
+  away_players = game_hash.fetch(:away).fetch(:players)
+  home_players + away_players
+end
+
+def team_names
+  teams.map do |team|
+    team[:team_name]
   end
+end
+
+def player_numbers(team_name)
+  find_the_team(team_name)[:players].map do |player|
+    player[:number]
+  end
+end
+
+def player_stats(player_name)
+  find_the_player(player_name).reject { |key, value| key == :player_name }
+end
+
+def find_the_player(name)
+  players.find {|player| player.fetch(:player_name) == name}
+end
